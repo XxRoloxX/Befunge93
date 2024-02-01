f539ef786e2ffc7d434d08d927ee19b588ed56da
Wieslaw <al.lo3opole@gmail.com>
2024-02-01 22:34:20 +0100

    Fix tests path

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,8 +3,9 @@ use befunge93::Interpreter;
 use std::fs::read_to_string;
 mod mocks;
 
-fn generic_test(input: Vec<u8>, expected_output: &str, file_path: &str) {
-    let funge_space = read_to_string(file_path);
+fn generic_test(input: Vec<u8>, expected_output: &str, file_name: &str) {
+    let full_path = format!("tests/befunge_programs/{}", file_name);
+    let funge_space = read_to_string(full_path);
     let output = MockOutput::boxed_new();
     let input = MockInput::boxed_new(input);
 
@@ -25,46 +26,46 @@ fn generic_test(input: Vec<u8>, expected_output: &str, file_path: &str) {
 
 #[test]
 fn big_factorial() {
-    generic_test(vec![b'9' as u8], "362880", "./tests/factorial.bf");
+    generic_test(vec![b'9' as u8], "362880", "factorial.bf");
 }
 
 #[test]
 fn small_factorial() {
-    generic_test(vec![b'3' as u8], "6", "./tests/factorial.bf");
+    generic_test(vec![b'3' as u8], "6", "factorial.bf");
 }
 
 #[test]
 fn hello_world_1() {
-    generic_test(vec![], "Hello World!", "./tests/hello_world_1.bf");
+    generic_test(vec![], "Hello World!", "hello_world_1.bf");
 }
 
 #[test]
 fn hello_world_2() {
-    generic_test(vec![], "Hello World!", "./tests/hello_world_2.bf");
+    generic_test(vec![], "Hello World!", "hello_world_2.bf");
 }
 
 #[test]
 fn hello_world_3() {
-    generic_test(vec![], "Hello World!", "./tests/hello_world_3.bf");
+    generic_test(vec![], "Hello World!", "hello_world_3.bf");
 }
 
 #[test]
 fn comparison_1() {
-    generic_test(vec![], "1", "./tests/comparison_1.bf");
+    generic_test(vec![], "1", "comparison_1.bf");
 }
 
 #[test]
 fn comparison_2() {
-    generic_test(vec![], "0", "./tests/comparison_2.bf");
+    generic_test(vec![], "0", "comparison_2.bf");
 }
 #[test]
 fn self_modification_1(){
-    generic_test(vec![], "x", "./tests/self_modification_1.bf")
+    generic_test(vec![], "x", "self_modification_1.bf")
 }
 
 #[test]
 fn self_modification_2(){
-    generic_test(vec![], "", "./tests/self_modification_2.bf")
+    generic_test(vec![], "", "self_modification_2.bf")
 }
 // #[test]
 // fn calc_1(){
