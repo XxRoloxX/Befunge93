02948f50c702f511a5018b6b094977a42b4ebd0a
Wieslaw <al.lo3opole@gmail.com>
2024-02-03 23:38:35 +0100

    Refactor CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,23 @@
 use clap::Parser;
+use crate::{io::FileOutput, InterpreterOutput};
+
 
 #[derive(Parser)]
 pub struct Cli {
-    pub path: std::path::PathBuf
+    pub program_path: std::path::PathBuf,
+    // to the output file
+    #[arg(short = 'o', long = "output")]
+    pub output_path: Option<std::path::PathBuf>,
+    //Path to the input file
+    #[arg(short = 'i', long = "input")]
+    pub input_path: Option<std::path::PathBuf>,
 }
+impl Cli {
+    pub fn output_buffer(&self) -> Option<InterpreterOutput> {
+        match &self.output_path {
+        Some(path) => Some(FileOutput::boxed_new(path.to_str().unwrap())),
+        None => None,
+    }
+}
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,16 @@ use befunge93::Interpreter;
 use clap::Parser;
 use std::fs::read_to_string;
 extern crate pretty_env_logger;
-use befunge93::io::FileOutput;
 
 fn main() {
-    let args = Cli::parse();
     pretty_env_logger::init();
-    let funge_space = read_to_string(&args.path);
+    let args = Cli::parse();
+    let funge_space = read_to_string(&args.program_path);
+    Interpreter::new(
+        &funge_space.unwrap(), 
+        None,
+        args.output_buffer()
+        ).run();
 
-    match funge_space {
-        Ok(res) => {
-            let mut funge_space =
-                Interpreter::new(&res, None, Some(FileOutput::boxed_new("./output.txt")));
-            funge_space.run();
-        }
-        Err(err) => {
-            print!("{}", err);
-        }
-    }
+
 }
